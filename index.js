4bc146e49f19a240279093a981894f5b61b1fde3
Alan Liang <liangalann@gmail.com>
2018-03-22 17:45:50 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function theBeatlesPlay(musicians, instruments) {
+  var array= [];
+  for (let i = 0; i < musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+    return array;
+}
+
+theBeatlesPlay(musicians, instruments)
+
+function johnLennonFacts(facts) {
+  while() {
+    console.log(facts + "!!!");
+  }
+  return facts
+}
